82bf157ab8d86c3eb40a9d1bb75bd1cd9bd67217
denis <denurchenko1981@gmail.com>
2022-07-01 19:12:01 +0300

    lang end

---
+++ dist/js/lang.js
@@ -0,0 +1,19 @@
+const langArr = {
+    'unit': {
+        'ua': 'Мене звати Денис Юрченко',
+        'ru': 'Меня зовут Денис Юрченко',
+        'en': 'My name is Denis Yurchenko',
+
+    },
+    'subtitle': {
+        'ua': 'Я web-розробник з міста Херсон',
+        'ru': 'Я web-разработчик из города Херсон',
+        'en': 'I am a web developer from the city of Kherson',
+
+    },
+    'sidepanel': {
+        'ua': 'Соціальні мережі',
+        'ru': 'Социальные сети',
+        'en': 'Social network',
+    }
+};
