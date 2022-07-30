459db868b4846a740429ed8abcdb7e251e1071cd
denis <denurchenko1981@gmail.com>
2022-07-30 10:23:59 +0300

    end--section-price

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,7 @@ const humburger = document.querySelector('.humburger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close');
 
+
 humburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
